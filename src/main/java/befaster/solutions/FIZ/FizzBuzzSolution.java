593f239936ede54b1eca73d3104e3cc8fb5e1916
sbx_user1060 <sbx_user1060@ip-10-128-77-122.eu-west-2.compute.internal>
2018-09-09 14:05:41 +0000

    Sun Sep 09 14:05:41 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,15 +5,24 @@ import static java.lang.String.valueOf;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        if (number % 15 == 0) {
-            return "fizz buzz";
-        } else if ((number % 3 == 0) || (valueOf(number).contains("3"))) {
-            return "fizz";
+
+        boolean isFizz = false;
+        boolean isBuzz = false;
+
+        if ((number % 3 == 0) || (valueOf(number).contains("3"))) {
+            isFizz = true;
         } else if ((number % 5 == 0) || (valueOf(number).contains("5"))) {
+            isBuzz = true;
+        }
+
+        if (isBuzz && isFizz || (number % 15 == 0)) {
+            return "fizz buzz";
+        } else if (isBuzz) {
             return "buzz";
+        } else if (isFizz) {
+            return "fizz";
         }
         return valueOf(number);
-
     }
 
 }
