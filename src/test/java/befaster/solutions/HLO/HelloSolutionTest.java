b1100f0aa232df13f8a9718fe7928f70fd5db699
sbx_user1060 <sbx_user1060@ip-10-128-77-122.eu-west-2.compute.internal>
2018-09-09 13:42:02 +0000

    Sun Sep 09 13:42:02 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,5 +1,23 @@
-import static org.junit.Assert.*;
+package befaster.solutions.HLO;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelloSolutionTest {
 
+    private HelloSolution helloSolution;
+
+    @Before
+    public void setUp() throws Exception {
+        helloSolution = new HelloSolution();
+    }
+
+    @Test
+    public void shouldReturnHelloPaul() {
+        String result = helloSolution.hello("Paul");
+        assertEquals(result, "Hello Paul");
+
+    }
 }
